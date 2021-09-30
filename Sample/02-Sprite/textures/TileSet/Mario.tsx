78cf5d7ee5c708553dd75b6bac29d74efaa39b02
DRyu1412 <72917241+DRyu1412@users.noreply.github.com>
2021-09-30 19:38:44 +0700

    Add tilemap for 02-Sprite
    draw a 1 window map with Tiled

---
+++ Sample/02-Sprite/textures/TileSet/Mario.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Mario" tilewidth="16" tileheight="16" tilecount="924" columns="33">
+ <image source="../../../../../../Codes/TileSet/Mario.png" width="528" height="448"/>
+</tileset>
